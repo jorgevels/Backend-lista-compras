fcdbca03df47436970d80aba1daa45ce62a94c2c
jorgevels <jorgevelasquez006@hotmail.com>
2021-10-09 14:36:44 -0500

    Subiendo proyecto

---
+++ src/initialState.ts
@@ -0,0 +1,57 @@
+const  initialState  =  {
+  carrito : [ ] ,
+  productos : [
+    {
+      'id' : '1' ,
+      'title' : 'Arroz' ,
+      'precio' : 1500 ,
+    } ,
+    {
+      'id': '2',
+      'title': 'Leche',
+      'precio': 8500,
+    },
+    {
+      'id' : '3' ,
+      'title' : 'Cafe' ,
+      'precio' : 6000 ,
+    } ,
+    {
+      'id': '4',
+      'title': 'Chocolate',
+      'precio': 5400,
+    },
+    {
+      'id' : '5' ,
+      'title' : 'Mantequilla' ,
+      'precio' : 6500 ,
+    } ,
+    {
+      'id': '6',
+      'title': 'Jamon',
+      'precio': 1200,
+    },{
+      'id' : '7' ,
+      'title' : 'Queso' ,
+      'precio' : 3500 ,
+    } ,
+    {
+      'id': '8',
+      'title': 'Pan',
+      'precio': 2300,
+    },
+    {
+      'id' : '9' ,
+      'title' : 'Cereal' ,
+      'precio' : 1500 ,
+    } ,
+    {
+      'id': '10',
+      'title': 'Avena',
+      'precio': 1200,
+    },
+
+  ] ,
+};
+
+export default initialState
