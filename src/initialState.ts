91da3c3a01f4a9e90fad74569a77479ad7ad652c
jorgevels <jorgevelasquez006@hotmail.com>
2021-10-11 20:06:05 -0500

    Update cors

--- src/initialState.ts
+++ src/initialState.ts
@@ -1,57 +1,55 @@
 const  initialState  =  {
   
  
-
-
   products : [
     {
       'id' : '1' ,
       'title' : 'Arroz' ,
-      'precio' : 1500 ,
+      'price' : 1500 ,
     } ,
     {
       'id': '2',
       'title': 'Leche',
-      'precio': 8500,
+      'price': 8500,
     },
     {
       'id' : '3' ,
       'title' : 'Cafe' ,
-      'precio' : 6000 ,
+      'price' : 6000 ,
     } ,
     {
       'id': '4',
       'title': 'Chocolate',
-      'precio': 5400,
+      'price': 5400,
     },
     {
       'id' : '5' ,
       'title' : 'Mantequilla' ,
-      'precio' : 6500 ,
+      'price' : 6500 ,
     } ,
     {
       'id': '6',
       'title': 'Jamon',
-      'precio': 1200,
+      'price': 1200,
     },{
       'id' : '7' ,
       'title' : 'Queso' ,
-      'precio' : 3500 ,
+      'price' : 3500 ,
     } ,
     {
       'id': '8',
       'title': 'Pan',
-      'precio': 2300,
+      'price': 2300,
     },
     {
       'id' : '9' ,
       'title' : 'Cereal' ,
-      'precio' : 1500 ,
+      'price' : 1500 ,
     } ,
     {
       'id': '10',
       'title': 'Avena',
-      'precio': 1200,
+      'price': 1200,
     },
 
   ] ,
