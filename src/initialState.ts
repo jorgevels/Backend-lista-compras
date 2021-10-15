1dbb7efd7707b0cde64f8c497ef3b3fb8e0b2d5f
jorgevels <jorgevelasquez006@hotmail.com>
2021-10-15 15:11:05 -0500

    Update lista de precios

--- src/initialState.ts
+++ src/initialState.ts
@@ -4,17 +4,17 @@ const  initialState  =  {
     {
       'id' : '1' ,
       'title' : 'Arroz' ,
-      'price' : 1500 ,
+      'price' : 15000 ,
     } ,
     {
       'id': '2',
       'title': 'Leche',
-      'price': 8500,
+      'price': 850,
     },
     {
       'id' : '3' ,
       'title' : 'Cafe' ,
-      'price' : 6000 ,
+      'price' : 600000 ,
     } ,
     {
       'id': '4',
@@ -97,6 +97,105 @@ const  initialState  =  {
     },
     {
       'id': '20',
+      'title': 'Jabon',
+      'price': 1200,
+    },
+    {
+      'id' : '21' ,
+      'title' : 'Arroz' ,
+      'price' : 15000 ,
+    } ,
+    {
+      'id': '22',
+      'title': 'Leche',
+      'price': 850,
+    },
+    {
+      'id' : '23' ,
+      'title' : 'Cafe' ,
+      'price' : 600000 ,
+    } ,
+    {
+      'id': '24',
+      'title': 'Chocolate',
+      'price': 5400,
+    },
+    {
+      'id' : '25' ,
+      'title' : 'Mantequilla' ,
+      'price' : 6500 ,
+    } ,
+    {
+      'id': '26',
+      'title': 'Jamon',
+      'price': 1200,
+    },{
+      'id' : '27' ,
+      'title' : 'Queso' ,
+      'price' : 3500 ,
+    } ,
+    {
+      'id': '28',
+      'title': 'Pan',
+      'price': 2300,
+    },
+    {
+      'id' : '29' ,
+      'title' : 'Cereal' ,
+      'price' : 1500 ,
+    } ,
+    {
+      'id': '30',
+      'title': 'Avena',
+      'price': 1200,
+    },
+    {
+      'id': '31',
+      'title': 'Pastas',
+      'price': 1200,
+    },
+    {
+      'id': '32',
+      'title': 'Arepas',
+      'price': 1200,
+    },
+    {
+      'id': '33',
+      'title': 'Azucar',
+      'price': 1200,
+    },
+    {
+      'id': '34',
+      'title': 'Salsa de tomate',
+      'price': 1200,
+    },
+    {
+      'id': '35',
+      'title': 'Milo',
+      'price': 1200,
+    },
+    {
+      'id': '36',
+      'title': 'Platano',
+      'price': 1200,
+    },
+    {
+      'id': '37',
+      'title': 'Zanahioria',
+      'price': 1200,
+    },
+    {
+      'id': '38',
+      'title': 'Tohallas de cosina',
+      'price': 1200,
+    },
+    {
+      'id': '39',
+      'title': 'Crema dental',
+      'price': 1200,
+    },
+    {
+      'id': '40',
       'title': 'Jabon',
       'price': 1200,
     },
